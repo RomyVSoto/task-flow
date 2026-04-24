f95994ead16aa9ef7ebb1471a9f3fb82ba4cabaf
RomyVSoto <romyvs01@gmail.com>
2026-04-24 16:46:54 -0400

    Fixed async params related issues

--- src/app/(App)/board/[id]/page.tsx
+++ src/app/(App)/board/[id]/page.tsx
@@ -3,18 +3,18 @@ import { api } from "~/trpc/server";
 import { Button } from "~/components/ui/button";
 import BoardClient from "./BoardClient";
 
-export default async function BoardPage({ params }: { params: { id: string } }) {
-  const board = await api.board.getById({ id: params.id });
-  const tasks = await api.task.getByBoard({ boardId: params.id });
+export default async function BoardPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
   
-  if(!board) {
+  const board = await api.board.getById({ id })
+  const tasks = await api.task.getByBoard({ boardId: id })
+
+  if (!board) {
     return <div className="flex gap-2">
       Board not found
       <Button><ArrowLeft /> Back</Button>
-    </div>;
+    </div>
   }
-  
-  return (
-    <BoardClient board={board} tasks={tasks} />
-  );
+
+  return <BoardClient board={board} tasks={tasks} />
 }
